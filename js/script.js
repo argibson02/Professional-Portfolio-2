cb4468dfd54c0c31c700eeb2931af82c3754eb0b
Alex Gibson <argibson02@gmail.com>
2021-10-11 18:39:23 -0700

    Added descriptions

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//3D
+$("#img-3d").on("click", function () { 
+    $('#img-3d').attr('src', "./images/3d-active.gif");
+});
+/*
+$("#img-3d").on("mouseleave", function () { 
+    $('#img-3d').attr('src', "./images/3d-still.png");
+});
+*/
+
+//Weather 
+$("#img-weather").on("mouseenter", function () { 
+    $('#img-weather').attr('src', "./images/weather-active.gif");
+});
+
+$("#img-weather").on("mouseleave", function () { 
+    $('#img-weather').attr('src', "./images/weather-still.png");
+});
+
+//Garfield
+$("#img-garfield").on("mouseenter", function () { 
+    $('#img-garfield').attr('src', "./images/garfield-active.gif");
+});
+
+$("#img-garfield").on("mouseleave", function () { 
+    $('#img-garfield').attr('src', "./images/garfield-still.png");
+});
+
