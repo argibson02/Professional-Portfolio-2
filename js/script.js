1813ff6d362e445477dfe461366f25b4e0df43b6
Alex Gibson <argibson02@gmail.com>
2021-10-11 19:44:59 -0700

    Added site and repo links

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //3D
-$("#img-3d").on("click", function () { 
+/*
+$("#img-3d").on("mouseenter", function () { 
     $('#img-3d').attr('src', "./images/3d-active.gif");
 });
-/*
+
 $("#img-3d").on("mouseleave", function () { 
     $('#img-3d').attr('src', "./images/3d-still.png");
 });
-*/
 
 //Weather 
 $("#img-weather").on("mouseenter", function () { 
@@ -26,3 +26,4 @@ $("#img-garfield").on("mouseleave", function () {
     $('#img-garfield').attr('src', "./images/garfield-still.png");
 });
 
+*/
